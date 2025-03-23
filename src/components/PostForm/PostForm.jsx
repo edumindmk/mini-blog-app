2fb041c740917a30fd15acece6b6b53b7a6aedd4
skender mustafa <>
2025-03-23 13:04:21 +0100

    Defined PostFrom initial UI

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -1,7 +1,32 @@
+import styles from './PostForm.module.css';
+
 const PostFrom = () => {
   return (
-    <div>
-      <h1>Add New Post</h1>
+    <div className={styles.container}>
+      <h1>Add New Post Form</h1>
+      <form>
+        <div className={styles.inputContainer}>
+          <label>Title</label>
+          <input type='text' />
+        </div>
+
+        <div className={styles.inputContainer}>
+          <label>Description</label>
+          <textarea type='text' />
+        </div>
+
+        <div className={styles.inputContainer}>
+          <label>Date</label>
+          <input type='text' />
+        </div>
+
+        <div className={styles.inputContainer}>
+          <label>Image link</label>
+          <input type='text' />
+        </div>
+      </form>
+
+      <button>Add new post</button>
     </div>
   );
 };
