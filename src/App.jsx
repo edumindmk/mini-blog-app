0b6e0dbdd8813ade76da6b9de2434250a9b12ea1
skender mustafa <>
2025-03-23 12:48:30 +0100

    Firt commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import PostFrom from './components/PostForm/PostForm';
+
+function App() {
+  return (
+    <>
+      <PostFrom />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/PostForm/PostForm.jsx
@@ -0,0 +1,9 @@
+const PostFrom = () => {
+  return (
+    <div>
+      <h1>Add New Post</h1>
+    </div>
+  );
+};
+
+export default PostFrom;
